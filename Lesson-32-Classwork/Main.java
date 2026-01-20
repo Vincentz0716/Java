1d9d1977036edbf8be8b1867cc0c81f3d9106bd9
Vincentz0716 <vincentz64@nycstudents.net>
2026-01-20 14:44:07 +0000

    wasd

--- Lesson-32-Classwork/Main.java
+++ Lesson-32-Classwork/Main.java
@@ -1,56 +1,69 @@
 class Main {
+
   public static void main(String[] args) {
     (new Main()).init();
   }
-
-  void print(Object o) { System.out.println(o); }
-  void printt(Object o) { System.out.print(o); }
+  
+  void print(Object o){System.out.println(o);}
+  void printt(Object o){System.out.print(o);}
 
   void init() {
+    CarItem item1 = new CarItem("Laptop", 1000, true); // On sale
+    CarItem item2 = new CarItem("Headphones", 200, false); // Not on sale
+    CarItem item3 = new CarItem("Smartphone", 800, true); // On sale
+    
+    CarItem[] cartItems = {item1, item2, item3};
+    
+    for (CarItem item : cartItems) {
+      if (item.isOnSale()) {
+        print("Item on sale: " + item.getName() + " - Price: " + item.getItemPrice());
+      }
+    }
 
-    double[] g1 = {60, 70, 90};
-    double[] g2 = {65, 75, 60};
-
-    String[] courses1 = {"EEFG7QH", "FSFC3", "HQFC3TH", "HUF43X"};
-    String[] courses2 = {"MRF43", "PGF43Q3C", "SCF43QG", "HUF43X"};
-
-    // Create two student objects
-    Student s1 = new Student("Alice", g1, courses1);
-    Student s2 = new Student("Bob", g2, courses2);
-
-    // Test checkCourse method
-    print("Alice taking HUF43X? " + s1.checkCourse("HUF43X"));
-    print("Alice taking MRF43? " + s1.checkCourse("MRF43"));
-
-    print("Bob taking HUF43X? " + s2.checkCourse("HUF43X"));
-    print("Bob taking FSFC3? " + s2.checkCourse("FSFC3"));
+    double subtotal = 0;
+    for (CarItem item : cartItems) {
+      subtotal += item.getItemPrice();
+    }
+    
+    double taxRate = 8.675 / 100; // Tax rate of 8.675%
+    double tax = subtotal * taxRate;
+    double totalAmount = subtotal + tax;
+    
+    print("Subtotal: " + subtotal);
+    print("Tax: " + tax);
+    print("Total amount: " + totalAmount);
   }
 
-  int randInt(int lower, int upper) {
+  int randInt(int lower, int upper){
     int range = upper - lower;
-    return (int)(Math.random() * range + lower);
+    return (int)(Math.random()*range + lower);
   }
 }
 
-class Student {
-  String name;
-  double[] grades;
-  String[] courses;
+class CarItem {
+  private String name;
+  private double itemPrice;
+  private boolean onSale;
 
-  
-  Student(String name, double[] grades, String[] courses) {
+  public CarItem(String name, double itemPrice, boolean onSale) {
     this.name = name;
-    this.grades = grades;
-    this.courses = courses;
+    this.itemPrice = itemPrice;
+    this.onSale = onSale;
   }
 
-  // Problem 2: checkCourse method
-  boolean checkCourse(String course) {
-    for (int i = 0; i < courses.length; i++) {
-      if (courses[i].equals(course)) {
-        return true;
-      }
+  public String getName() {
+    return name;
+  }
+
+  public double getItemPrice() {
+    if (onSale) {
+      return itemPrice * 0.9; 
+    } else {
+      return itemPrice;
     }
-    return false;
+  }
+
+  public boolean isOnSale() {
+    return onSale;
   }
 }
