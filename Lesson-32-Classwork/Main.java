52d750bc4f10dc0f71f4cee9a8f169c0a5531bd1
Vincentz0716 <vincentz64@nycstudents.net>
2026-01-21 15:10:19 +0000

    wasddsad

--- Lesson-32-Classwork/Main.java
+++ Lesson-32-Classwork/Main.java
@@ -1,69 +1,63 @@
 class Main {
-
   public static void main(String[] args) {
     (new Main()).init();
   }
-  
-  void print(Object o){System.out.println(o);}
-  void printt(Object o){System.out.print(o);}
+
+  void print(Object o) {
+    System.out.println(o);
+  }
 
   void init() {
-    CarItem item1 = new CarItem("Laptop", 1000, true); // On sale
-    CarItem item2 = new CarItem("Headphones", 200, false); // Not on sale
-    CarItem item3 = new CarItem("Smartphone", 800, true); // On sale
-    
-    CarItem[] cartItems = {item1, item2, item3};
-    
-    for (CarItem item : cartItems) {
-      if (item.isOnSale()) {
-        print("Item on sale: " + item.getName() + " - Price: " + item.getItemPrice());
+
+    double[] g1 = {60, 70, 90};
+    double[] g2 = {65, 75, 60};
+    double[] g3 = {78, 70, 80};
+    double[] g4 = {64, 77, 90};
+    double[] g5 = {77, 76, 70};
+
+    String[] courses1 = {"EEFG7QH", "FSFC3", "HQFC3TH", "HUF43X"};
+    String[] courses2 = {"MRF43", "PGF43Q3C", "SCF43QG", "HUF43X"};
+    String[] courses3 = {"MRF43", "PGF43Q3C", "SCF43QG", "HUF43X"};
+    String[] courses4 = {"MRF43", "PGF43Q3C", "SCF43QG", "HUF43X"};
+    String[] courses5 = {"MRF43", "PGF43Q3C", "SCF43QG", "HUF43X"};
+
+    // Calculate averages FIRST
+    double avg1 = calcAvg(g1);
+    double avg2 = calcAvg(g2);
+    double avg3 = calcAvg(g3);
+    double avg4 = calcAvg(g4);
+    double avg5 = calcAvg(g5);
+
+    // Create array
+    Student[] studentList = new Student[5];
+
+    // PASS averages (double), NOT arrays
+    studentList[0] = new Student("Tom", 9, avg1, courses1);
+    studentList[1] = new Student("Jerry", 11, avg2, courses2);
+    studentList[2] = new Student("Ren", 10, avg3, courses3);
+    studentList[3] = new Student("Stimpy", 9, avg4, courses4);
+    studentList[4] = new Student("Krusty", 9, avg5, courses5);
+
+    // Problem 1 & 2
+    double[] avgs = {avg1, avg2, avg3, avg4, avg5};
+    int above70 = 0;
+
+    for (int i = 0; i < avgs.length; i++) {
+      print("Student " + (i + 1) + " average: " + avgs[i]);
+
+      if (avgs[i] > 70) {
+        above70++;
       }
     }
 
-    double subtotal = 0;
-    for (CarItem item : cartItems) {
-      subtotal += item.getItemPrice();
+    print("Number of students with average above 70: " + above70);
+  }
+
+  double calcAvg(double[] grades) {
+    double sum = 0;
+    for (double g : grades) {
+      sum += g;
     }
-    
-    double taxRate = 8.675 / 100; // Tax rate of 8.675%
-    double tax = subtotal * taxRate;
-    double totalAmount = subtotal + tax;
-    
-    print("Subtotal: " + subtotal);
-    print("Tax: " + tax);
-    print("Total amount: " + totalAmount);
-  }
-
-  int randInt(int lower, int upper){
-    int range = upper - lower;
-    return (int)(Math.random()*range + lower);
-  }
-}
-
-class CarItem {
-  private String name;
-  private double itemPrice;
-  private boolean onSale;
-
-  public CarItem(String name, double itemPrice, boolean onSale) {
-    this.name = name;
-    this.itemPrice = itemPrice;
-    this.onSale = onSale;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public double getItemPrice() {
-    if (onSale) {
-      return itemPrice * 0.9; 
-    } else {
-      return itemPrice;
-    }
-  }
-
-  public boolean isOnSale() {
-    return onSale;
+    return sum / grades.length;
   }
 }
